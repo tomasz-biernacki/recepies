faf31a9f8bb7ac109c74eecd276329a6cb2ae873
Tomasz Biernacki <tomek901@gmail.com>
2016-07-22 16:05:22 +0200

    Display fake data

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, jsonify
 import time #simulating slow calculation
 
 app = Flask(__name__)
+
+fake_ingredients = ["100g kurczaka", "200g papryk", "50g piwa"]
+fake_descriptions = ["Place the zucchini, bell pepper, and pineapple in a mixing bowl. Drizzle with olive oil, and toss to coat. Divide the mixture into freezer bags. Whisk the ketchup, salt, steak sauce, sugar, vinegar, Worcestershire sauce, and water together in the same bowl until smooth. Add the beef cubes, and toss until evenly coated. Divide the beef into freezer bags. Seal, and freeze the bags.", "Place the zucchini, bell pepper, and pineapple in a mixing bowl. Drizzle with olive oil, and toss to coat. Divide the mixture into freezer bags. Whisk the ketchup, salt, steak sauce, sugar, vinegar, Worcestershire sauce, and water together in the same bowl until smooth. Add the beef cubes, and toss until evenly coated. Divide the beef into freezer bags. Seal, and freeze the bags."]
 
 @app.route('/')
 def hello():
@@ -10,9 +13,9 @@ def hello():
 
 @app.route('/fetch')
 def fetch_recipe():
-    time.sleep(3)
-    return "Test"
-
+    time.sleep(1)
+    return jsonify(ingredients=fake_ingredients,
+                   descriptions=fake_descriptions)
 
 if __name__ == "__main__":
     app.run()
--- static/main.js
+++ static/main.js
@@ -1,15 +1,37 @@
 var loadingDiv = document.getElementById('loading');
 var outputDiv = document.getElementById('output');
+var ingredientsDiv = document.getElementById('ingredients');
+var descriptionDiv = document.getElementById('description');
+
+function build_ingredients(ingredients) {
+  ingredientsDiv.innerHTML = "";
+  ingredients.forEach(function(item, index) {
+    ingredientsDiv.innerHTML += "<div class='ingredients-item'>" + item + "</div>";
+  });
+}
+
+function build_description(descriptions) {
+  descriptionDiv.innerHTML = "";
+  descriptions.forEach(function(item, index) {
+    descriptionDiv.innerHTML += "<div class='description-item'>" + item + "</div>";
+  })
+}
+
+function build_recipe (data) {
+  var ingredients = data['ingredients'];
+  build_ingredients(ingredients);
+  var descriptions = data['descriptions'];
+  build_description(descriptions);
+}
 
 function fetch_data () {
   loadingDiv.style.display = 'block';
-  output.innerHTML = "";
 
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       loadingDiv.style.display = 'none';
-      output.innerHTML = xhttp.responseText;
+      build_recipe(JSON.parse(xhttp.responseText));
     }
   };
   xhttp.open("GET", "/fetch", true);
