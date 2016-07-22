ae59e3984e2472391f46b5ac2e70f6d26c43b61a
Tomasz Biernacki <tomek901@gmail.com>
2016-07-22 12:40:23 +0200

    Basic spash page

--- hello.py
+++ hello.py
@@ -3,9 +3,9 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/')
+def hello():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
