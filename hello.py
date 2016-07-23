108d308e4291ac40d6392645783ae489839e4e36
Tomasz Biernacki <tomek901@gmail.com>
2016-07-23 13:06:23 +0200

    Read files and format

--- hello.py
+++ hello.py
@@ -1,30 +1,41 @@
 from flask import Flask
 from flask import render_template, jsonify
-import time #simulating slow calculation
-import subprocess
 from random import randint
+import time #simulating slow calculation
 
 app = Flask(__name__)
 
-description = []
-ingredients = []
-
 def read_rand_file():
     r = randint(1, 50)
-    r = 1 #uncomment
     file_name = "sample_" + str(r)
+    print "READING " + file_name
     content = ""
     with open('samples/' + file_name, 'r') as content_file:
         content = content_file.read()
     return content
 
 def modify_output_recipie(nn_output):
-    recipie = nn_output.split('\n',2)[2]
-    recipie = recipie.split('<!PRZEPIS>')
-    raw_ingredients = recipie[0].split(',')
-    raw_description = recipie[1]
-    description.append(raw_description)
-    ingredients.extend(raw_ingredients)
+    description = []
+    ingredients = []
+    splitted = nn_output.split('\n')
+    ing = False
+    steps = False
+    for l in splitted:
+        if l == "<!SKLADNIKI>:":
+            ing = True
+            continue
+        if l == "<!PRZEPIS>":
+            ing = False
+            steps = True
+            continue
+        if l == "<!STOP>":
+            break
+        if ing == True:
+            ingredients.append(l)
+        if steps == True:
+            description.append(l)
+    return jsonify(ingredients=ingredients,
+                   descriptions=description)
 
 @app.route('/')
 def hello():
@@ -33,9 +44,7 @@ def hello():
 @app.route('/fetch')
 def fetch_recipe():
     time.sleep(1)
-    print read_rand_file()
-    return jsonify(ingredients=ingredients,
-                   descriptions=description)
+    return modify_output_recipie(read_rand_file())
 
 if __name__ == "__main__":
     app.run()
